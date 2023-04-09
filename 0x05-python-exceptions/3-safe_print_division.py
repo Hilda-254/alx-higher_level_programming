4d069d16fdbd0ccc16e9ebd197b2c84e7f063bad
Hilda-254 <hildamunjuri@gmail.com>
2023-04-09 02:27:21 -0700

    Safe print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,13 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
-        print("Cannot divide by zero")
-        return None
-    except TypeError:
-        print("Both inputs must be integers")
-        return None
-    else:
-        return result
+    except (TypeError, ZeroDivisionError):
+        result = None
     finally:
         print("Inside result: {}".format(result))
+    return (result)
