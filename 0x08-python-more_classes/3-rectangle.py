e69742f6cdbb64b5beb5eea5040980faffb64867
Hilda-254 <hildamunjuri@gmail.com>
2023-04-13 01:26:43 -0700

    String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+# 2-rectangle.py by HILDA MUNJURI
+"""A class Rectangle that defines a rectangle by: (based on 1-rectangle.py)"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new rectangle.
+        Args:
+            width:The width of the rectangle.
+            height:The height of the rectangle.
+        Raises:
+            TypeError: If value is not integer
+            ValueError: If value is less than zero
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets the current width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Get the current height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Calculates the area of the rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculates the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)  
+
+    rect = Rectangle(width, height)
+    for i in range(rect.width):
+        for j in range(rect.height):
+            print(str('#'), end='')
+            print()
