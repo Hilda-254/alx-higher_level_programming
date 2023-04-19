c1c451b6baf44883db3479bf798415591820d7bd
Hilda-254 <hildamunjuri@gmail.com>
2023-04-19 02:48:23 -0700

    Base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A class BaseGeometry (based on 5-base_geometry.py)"""
+
+
+class BaseGeometry:
+    """Base of surface on which a shape stands on."""
+
+    def area(self):
+        """Not used"""
+        raise Exception ("area() is not implemented")
