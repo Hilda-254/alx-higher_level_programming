87a48a71157b75b1229f7f96af90c5ad79dc711d
Hilda-254 <hildamunjuri@gmail.com>
2023-05-09 02:57:52 -0700

    Matrix divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-"""This module defines a matrix division function
-"""
-
-
 def matrix_divided(matrix, div):
-    """This function divides all the elements of a matrix by a given number
-    Args:
-        matrix: A list of lists (matrix)- members can be of type ints or floats
-        div: Number to be used for the division (can be a float or an integer)
-    Raises:
-        TypeError: If the matrix has non-numbers
-        TypeError: If the matrix has rows of different sizes
-        TypeError: If div is not an int or float
-        ZeroDivisionError: If div is 0
-    Returns:
-        A new matrix which represents the result of the divisions
-    """
-    if not all(isinstance(row, list) for row in matrix) or
-    not all(isinstance(num, (int, float)) for row in matrix for num in row):
-        raise TypeError("matrix must be a matrix (list of lists) of"
-                        "integers/floats")
-    if len(set(len(row) for row in matrix)) != 1:
+    """A function that divides all elements of a matrix."""
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix) or \
+            not all(isinstance(num, (int, float)) for row in matrix for num in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    row_size = len(matrix[0])
+    if not all(len(row) == row_size for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    new_matrix = [[round(num/div, 2) for num in row] for row in matrix]
-    return new_matrix
+
+    divided_matrix = [[round(num / div, 2) for num in row] for row in matrix]
+
+    return divided_matrix
