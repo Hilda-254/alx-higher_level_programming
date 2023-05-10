1b27e08bd3051850fa55c0f511f37833a13e27a0
Hilda-254 <hildamunjuri@gmail.com>
2023-05-09 23:10:41 -0700

    Max integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,30 +1,38 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([...])
+"""
 import unittest
-from max_integer_test import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
-class MaxIntegerTest(unittest.TestCase):
+class TestMaxInteger(unittest.TestCase):
     def test_empty_list(self):
-        result = max_integer([])
-        self.assertIsNone(result)
+        """Test max_integer with an empty list"""
+        self.assertEqual(max_integer([]), None)
 
     def test_single_element(self):
-        result = max_integer([5])
-        self.assertEqual(result, 5)
+        """Test max_integer with a list containing a single element"""
+        self.assertEqual(max_integer([5]), 5)
 
     def test_positive_numbers(self):
-        result = max_integer([1, 2, 3, 4, 5])
-        self.assertEqual(result, 5)
+        """Test max_integer with a list of positive numbers"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_negative_numbers(self):
-        result = max_integer([-5, -4, -3, -2, -1])
-        self.assertEqual(result, -1)
+        """Test max_integer with a list of negative numbers"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
     def test_mixed_numbers(self):
-        result = max_integer([-2, 0, 5, -10, 3])
-        self.assertEqual(result, 5)
+        """Test max_integer with a list of mixed positive and negative numbers"""
+        self.assertEqual(max_integer([-5, 0, 10, -2]), 10)
 
     def test_duplicate_numbers(self):
-        result = max_integer([3, 3, 3, 3])
-        self.assertEqual(result, 3)
+        """Test max_integer with a list containing duplicate numbers"""
+        self.assertEqual(max_integer([1, 2, 2, 3, 3, 3]), 3)
+
+    def test_large_numbers(self):
+        """Test max_integer with a list of large numbers"""
+        self.assertEqual(max_integer([1000000, 2000000, 3000000]), 3000000)
 
 if __name__ == '__main__':
     unittest.main()
+
