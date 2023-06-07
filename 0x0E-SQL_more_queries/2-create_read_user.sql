e81d796ef116bb7cbd67e4013b72889801c5a24d
Hilda-254 <hildamunjuri@gmail.com>
2023-06-07 07:06:52 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,7 @@
 -- A script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- create hbtn_0d_2 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create user_0d_2
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT TO user_0d_2@localhost ON hbtn_0d_2;
+-- grant SELECT privileges to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
