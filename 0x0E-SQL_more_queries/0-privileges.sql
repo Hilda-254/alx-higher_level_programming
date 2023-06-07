185f26ce6b85e520ca7b189711890af01141ecc7
Hilda-254 <hildamunjuri@gmail.com>
2023-06-07 06:11:23 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 /*A script shows MySQL server priviledge for user_0d_1 and user_0d_2*/
-SELECT CONCAT ('SHOW GRANTS FOR' 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';) FROM mysql.user;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
